4d2754aedf58300fbac214693dcc21259742324c
cielivree <cielivree93@gmail.com>
2026-02-09 20:07:32 +0200

    Post new message

--- server/long_pulling.js
+++ server/long_pulling.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const cors = require('cors')
+const events = require('events')
 
 const PORT = 5000
 
+const emitter = events.EventEmitter()
+
 const app = express()
+app.use(cors())
+
+app.get('get-messages', (request, response) => { })
+
+app.post('new-messages', (request, response) => {
+    const message = request.body
+    response.status(200)
+})
+
 app.listen(PORT, () => console.log(`Server started on ${PORT} port`))
 
