57b78d0d3e4f9b0a9f34762d736e1cefdae00d09
cielivree <cielivree93@gmail.com>
2026-02-09 22:34:18 +0200

    Server side long-pulling

--- server/long_pulling.js
+++ server/long_pulling.js
@@ -9,10 +9,15 @@ const emitter = events.EventEmitter()
 const app = express()
 app.use(cors())
 
-app.get('get-messages', (request, response) => { })
+app.get('get-messages', (request, response) => {
+    emitter.once('newMessage', (message) => {
+        response.json(message)
+    })
+})
 
 app.post('new-messages', (request, response) => {
     const message = request.body
+    emitter.emit('newMessage', message)
     response.status(200)
 })
 
