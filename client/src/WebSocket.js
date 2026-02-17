69cb437d1ecfc6fa073af76e4a6135af46bbbfa1
cielivree <cielivree93@gmail.com>
2026-02-17 17:31:57 +0200

    Websocket server side

---
+++ client/src/WebSocket.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const LongPulling = () => {
+    const [messages, setMessages] = useState([])
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        subscribe()
+    }, [])
+
+    const subscribe = async () => {
+        const eventSource = new EventSource('http://localhost:5000/connect')
+        eventSource.onmessage = function (event) {
+            const message = JSON.parse(event.data)    // convert string back into the object
+            setMessages(prev => [message, ...prev])
+        }
+    }
+
+    const sendMessage = async () => {
+        await axios.post('http://localhost:5000/new-messages', {
+            message: value,
+            id: Date.now()
+        })
+    }
+
+  return (
+    <div className="center">
+        <div>
+            <div className='form'>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
+                <button onClick={sendMessage}>Send</button>
+            </div>
+            <div className="messages">
+                {messages.map(mes => 
+                    <div className='message' key={mes.id}>
+                        {mes.message} 
+                    </div>
+                )}
+            </div>
+         </div>
+    </div>
+  )
+}
+
+export default LongPulling
--- server/websocket.js
+++ server/websocket.js
@@ -5,3 +5,33 @@ const wsServer = new ws.Server({
 }, () => {
    console.log('Server started by websocket') 
 })
+
+wsServer.on('connection', function connection(ws) {
+    ws.send('User1 has successfully connected')
+    
+    ws.on('message', function (message) {
+        message = JSON.parse(message)
+        switch (message.event) {
+            case 'message':
+                broadcastMessage(message)
+                break;
+            case 'connection':
+                broadcastMessage(message)
+                break;
+        }
+    })
+})
+
+const message = {
+    event: 'message' || 'connection',
+    id: 123,
+    date: '17.02.2026',
+    username: 'Kitty',
+    message: 'Hello, everyone!'
+}
+
+function broadcastMessage(message) {
+    wsServer.clients.forEach(client => {
+        client.send(JSON.stringify(message))
+    })
+}
